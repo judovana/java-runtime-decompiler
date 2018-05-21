e54e4d52029d665df10c9bf297df3539384e8a36
Radek Maňák <radek.manak@protonmail.com>
2018-05-21 11:23:20 +0200

    improve vmlistRender

--- src/main/java/com/redhat/thermostat/vm/decompiler/swing/VmListRenderer.java
+++ src/main/java/com/redhat/thermostat/vm/decompiler/swing/VmListRenderer.java
@@ -14,18 +14,28 @@ public class VmListRenderer extends JPanel implements ListCellRenderer {
     public VmListRenderer() {
         pid = new JLabel();
         name = new JLabel();
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        this.add(name, gbc);
+        gbc.weighty = 0.75;
+        gbc.gridy = 1;
+        this.add(pid, gbc);
         this.setBorder(new EtchedBorder(1));
 
-        this.add(pid);
-        this.add(name);
     }
 
     @Override
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         VmInfo vminfo = (VmInfo)value;
-        pid.setText(vminfo.getVmId());
-        name.setText(vminfo.getVmName().split(" ")[0]);
-        name.setToolTipText(vminfo.getVmName());
+        pid.setText("   PID: " + vminfo.getVmId());
+        name.setText("   " + vminfo.getVmName().split(" ")[0]);
+        this.setToolTipText("<html>NAME: " + vminfo.getVmName().split(" ")[0] + "<br>PID: " + vminfo.getVmId() + "</html>");
 
         if (isSelected){
             setBackground(list.getSelectionBackground());
