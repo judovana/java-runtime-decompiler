04da68c5c8c1699a1482e7752cba90f9b005e53f
Radek Maňák <radek.manak@protonmail.com>
2018-08-07 13:41:57 +0000

    create wrapper for Fernflower decompiler

---
+++ src/plugins/FernflowerDecompilerWrapper.java
@@ -0,0 +1,50 @@
+import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FernflowerDecompilerWrapper {
+
+    public String decompile(byte[] bytecode){
+        File tempByteFile = null;
+        File decompiledFile = null;
+        String decompiledString;
+        try {
+            tempByteFile = bytesToFile(bytecode);
+            String[] args = new String[]{tempByteFile.getAbsolutePath(), System.getProperty("java.io.tmpdir")};
+            ConsoleDecompiler.main(args);
+            String decompiledFilePath = tempByteFile.getPath().substring(0, tempByteFile.getPath().length()-6)+ ".java";
+            decompiledFile = new File(decompiledFilePath);
+            decompiledString = readStringFromFile(decompiledFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        } finally {
+            if (tempByteFile != null && tempByteFile.exists()){
+                tempByteFile.delete();
+            }
+            if (decompiledFile != null && decompiledFile.exists()){
+                decompiledFile.delete();
+            }
+            
+        }
+        return decompiledString;
+    }
+
+    private String readStringFromFile(String filePath) throws IOException {
+        String fileContent = new String(Files.readAllBytes(Paths.get(filePath)));
+        return fileContent;
+    }
+
+    private File bytesToFile(byte[] bytes) throws IOException {
+        File tempFile = File.createTempFile("temporary-byte-file", ".class");
+        tempFile.deleteOnExit();
+        FileOutputStream fos = new FileOutputStream(tempFile.getCanonicalPath());
+        fos.write(bytes);
+        fos.close();
+        return tempFile;
+    }
+}
