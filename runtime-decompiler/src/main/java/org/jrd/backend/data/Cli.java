3d3a9ce650ceb069078fb47c929189381e3f7447
Jiri <jvanek@redhat.com>
2018-11-10 20:13:34 +0100

    Cli refactored to have individual targets in separate methods

--- runtime-decompiler/src/main/java/org/jrd/backend/data/Cli.java
+++ runtime-decompiler/src/main/java/org/jrd/backend/data/Cli.java
@@ -7,6 +7,7 @@ import org.jrd.backend.decompiling.PluginManager;
 import org.jrd.frontend.VmDecompilerInformationController;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -76,121 +77,21 @@ public class Cli {
             String arg = args.get(i);
             arg = cleanParameter(arg);
             if (arg.equals(LISTJVMS)) {
-                if (args.size() != 1) {
-                    throw new RuntimeException(LISTJVMS + " do not expect argument");
-                }
-                for (VmInfo vm : manager.vmList) {
-                    System.out.println(vm.getVmPid() + " " + vm.getVmName());
-                }
+                listJvms(args);
                 break;
             }
             if (arg.equals(LISTPLUGINS)) {
-                if (args.size() != 1) {
-                    throw new RuntimeException(LISTPLUGINS + " do not expect argument");
-                }
-                PluginManager pm = new PluginManager();
-                List<DecompilerWrapperInformation> wrappers = pm.getWrappers();
-                for (DecompilerWrapperInformation dw : wrappers) {
-                    System.out.println(dw.getName() + " " + dw.getScope() + "/" + invalidityToString(dw.isInvalidWrapper()) + " - " + dw.getFileLocation());
-                }
+                listPlugins(args);
                 break;
             } else if (arg.equals(LISTCLASSES)) {
-                if (args.size() != 2) {
-                    throw new RuntimeException(LISTCLASSES + " expect exactly one argument - pid or url");
-                }
-                String param = args.get(i + 1);
-                try {
-                    VmInfo vmInfo = manager.findVmFromPID(param);
-                    AgentRequestAction request = VmDecompilerInformationController.createRequest(manager, vmInfo, null, AgentRequestAction.RequestAction.CLASSES);
-                    String response = VmDecompilerInformationController.submitRequest(manager, request);
-                    if (response.equals("ok")) {
-                        VmDecompilerStatus vmStatus = manager.getVmDecompilerStatus(vmInfo);
-                        String[] classes = vmStatus.getLoadedClassNames();
-                        for (String clazz : classes) {
-                            System.out.println(clazz);
-                        }
-                    }
-                    if (response.equals("error")) {
-                        throw new RuntimeException(VmDecompilerInformationController.CLASSES_NOPE);
-
-                    }
-                } catch (NumberFormatException e) {
-                    try {
-                        URL u = new URL(param);
-                        throw new RuntimeException("Remote VM not yet implemented");
-                    } catch (MalformedURLException ee) {
-                        throw new RuntimeException("Second param was supposed to be URL or PID", ee);
-                    }
-                }
+                listClasses(args, i);
                 break;
             } else if (arg.equals(BYTES) || arg.equals(BASE64)) {
-                if (args.size() != 3) {
-                    throw new RuntimeException(BYTES + " and " + BASE64 + " expect exactly two argument - pid or url of JVM and fully classified class name");
-                }
-                String jvmStr = args.get(i + 1);
-                String classStr = args.get(i + 2);
-                try {
-                    VmInfo vmInfo = manager.findVmFromPID(jvmStr);
-                    VmDecompilerStatus result = obtainClass(vmInfo, classStr, manager);
-                    if (arg.equals(BYTES)) {
-                        byte[] ba = Base64.getDecoder().decode(result.getLoadedClassBytes());
-                        System.out.write(ba);
-                    } else if (arg.equals(BASE64)) {
-                        System.out.println(result.getLoadedClassBytes());
-                    } else {
-                        throw new RuntimeException("Moon had fallen to Earth and Sun burned the rest...");
-                    }
-                } catch (NumberFormatException e) {
-                    try {
-                        URL u = new URL(jvmStr);
-                        throw new RuntimeException("Remote VM not yet implemented");
-                    } catch (MalformedURLException ee) {
-                        throw new RuntimeException("Second param was supposed to be URL or PID", ee);
-                    }
-                }
+                boolean bytes = arg.equals(BYTES);
+                printBytes(args, i, bytes);
                 break;
             } else if (arg.equals(DECOMPILE)) {
-                if (args.size() != 4) {
-                    throw new RuntimeException(DECOMPILE + " expect exactly three argument - pid or url of JVM, fully classified class name and decompiler name (as set-up) or decompiler json file");
-                }
-                String jvmStr = args.get(i + 1);
-                String classStr = args.get(i + 2);
-                String decompilerName = args.get(i + 3);
-                try {
-                    VmInfo vmInfo = manager.findVmFromPID(jvmStr);
-                    VmDecompilerStatus result = obtainClass(vmInfo, classStr, manager);
-                    byte[] bytes = Base64.getDecoder().decode(result.getLoadedClassBytes());
-                    PluginManager pluginManager = new PluginManager();
-                    if (new File(decompilerName).exists() && decompilerName.toLowerCase().endsWith(".json")) {
-                        throw new RuntimeException("Plugins laoded directly form file are noty impelemnetd");
-                    }
-                    List<DecompilerWrapperInformation> wrappers = pluginManager.getWrappers();
-                    DecompilerWrapperInformation decompiler = null;
-                    for (DecompilerWrapperInformation dw : wrappers) {
-                        if (!dw.getScope().equals(DecompilerWrapperInformation.LOCAL_SCOPE) && dw.getName().equals(decompilerName)) {
-                            decompiler = dw;
-                        }
-                    }
-                    //LOCAL is preffered one
-                    for (DecompilerWrapperInformation dw : wrappers) {
-                        if (dw.getScope().equals(DecompilerWrapperInformation.LOCAL_SCOPE) && dw.getName().equals(decompilerName)) {
-                            decompiler = dw;
-                        }
-                    }
-                    if (decompiler != null) {
-                        String decompiledClass = pluginManager.decompile(decompiler, bytes);
-                        System.out.println(decompiledClass);
-                    } else {
-                        throw new RuntimeException("Decompiler " + decompilerName + " not found");
-                    }
-                } catch (NumberFormatException e) {
-                    try {
-                        URL u = new URL(jvmStr);
-                        throw new RuntimeException("Remote VM not yet implemented");
-                    } catch (MalformedURLException ee) {
-                        throw new RuntimeException("Second param was supposed to be URL or PID", ee);
-                    }
-                }
+                decompile(args, i);
                 break;
             } else {
                 printHelp();
@@ -199,16 +100,135 @@ public class Cli {
         }
     }
 
+    private void decompile(List<String> args, int i) throws Exception {
+        if (args.size() != 4) {
+            throw new RuntimeException(DECOMPILE + " expect exactly three argument - pid or url of JVM, fully classified class name and decompiler name (as set-up) or decompiler json file");
+        }
+        String jvmStr = args.get(i + 1);
+        String classStr = args.get(i + 2);
+        String decompilerName = args.get(i + 3);
+        try {
+            VmInfo vmInfo = manager.findVmFromPID(jvmStr);
+            VmDecompilerStatus result = obtainClass(vmInfo, classStr, manager);
+            byte[] bytes = Base64.getDecoder().decode(result.getLoadedClassBytes());
+            PluginManager pluginManager = new PluginManager();
+            if (new File(decompilerName).exists() && decompilerName.toLowerCase().endsWith(".json")) {
+                throw new RuntimeException("Plugins laoded directly form file are noty impelemnetd");
+            }
+            List<DecompilerWrapperInformation> wrappers = pluginManager.getWrappers();
+            DecompilerWrapperInformation decompiler = null;
+            for (DecompilerWrapperInformation dw : wrappers) {
+                if (!dw.getScope().equals(DecompilerWrapperInformation.LOCAL_SCOPE) && dw.getName().equals(decompilerName)) {
+                    decompiler = dw;
+                }
+            }
+            //LOCAL is preffered one
+            for (DecompilerWrapperInformation dw : wrappers) {
+                if (dw.getScope().equals(DecompilerWrapperInformation.LOCAL_SCOPE) && dw.getName().equals(decompilerName)) {
+                    decompiler = dw;
+                }
+            }
+            if (decompiler != null) {
+                String decompiledClass = pluginManager.decompile(decompiler, bytes);
+                System.out.println(decompiledClass);
+            } else {
+                throw new RuntimeException("Decompiler " + decompilerName + " not found");
+            }
+        } catch (NumberFormatException e) {
+            try {
+                URL u = new URL(jvmStr);
+                throw new RuntimeException("Remote VM not yet implemented");
+            } catch (MalformedURLException ee) {
+                throw new RuntimeException("Second param was supposed to be URL or PID", ee);
+            }
+        }
+    }
+
+    private void printBytes(List<String> args, int i, boolean bytes) throws IOException {
+        if (args.size() != 3) {
+            throw new RuntimeException(BYTES + " and " + BASE64 + " expect exactly two argument - pid or url of JVM and fully classified class name");
+        }
+        String jvmStr = args.get(i + 1);
+        String classStr = args.get(i + 2);
+        try {
+            VmInfo vmInfo = manager.findVmFromPID(jvmStr);
+            VmDecompilerStatus result = obtainClass(vmInfo, classStr, manager);
+            if (bytes) {
+                byte[] ba = Base64.getDecoder().decode(result.getLoadedClassBytes());
+                System.out.write(ba);
+            } else {
+                System.out.println(result.getLoadedClassBytes());
+            }
+        } catch (NumberFormatException e) {
+            try {
+                URL u = new URL(jvmStr);
+                throw new RuntimeException("Remote VM not yet implemented");
+            } catch (MalformedURLException ee) {
+                throw new RuntimeException("Second param was supposed to be URL or PID", ee);
+            }
+        }
+    }
+
+    private void listClasses(List<String> args, int i) {
+        if (args.size() != 2) {
+            throw new RuntimeException(LISTCLASSES + " expect exactly one argument - pid or url");
+        }
+        String param = args.get(i + 1);
+        try {
+            VmInfo vmInfo = manager.findVmFromPID(param);
+            AgentRequestAction request = VmDecompilerInformationController.createRequest(manager, vmInfo, null, AgentRequestAction.RequestAction.CLASSES);
+            String response = VmDecompilerInformationController.submitRequest(manager, request);
+            if (response.equals("ok")) {
+                VmDecompilerStatus vmStatus = manager.getVmDecompilerStatus(vmInfo);
+                String[] classes = vmStatus.getLoadedClassNames();
+                for (String clazz : classes) {
+                    System.out.println(clazz);
+                }
+            }
+            if (response.equals("error")) {
+                throw new RuntimeException(VmDecompilerInformationController.CLASSES_NOPE);
+
+            }
+        } catch (NumberFormatException e) {
+            try {
+                URL u = new URL(param);
+                throw new RuntimeException("Remote VM not yet implemented");
+            } catch (MalformedURLException ee) {
+                throw new RuntimeException("Second param was supposed to be URL or PID", ee);
+            }
+        }
+    }
+
+    private void listPlugins(List<String> args) {
+        if (args.size() != 1) {
+            throw new RuntimeException(LISTPLUGINS + " do not expect argument");
+        }
+        PluginManager pm = new PluginManager();
+        List<DecompilerWrapperInformation> wrappers = pm.getWrappers();
+        for (DecompilerWrapperInformation dw : wrappers) {
+            System.out.println(dw.getName() + " " + dw.getScope() + "/" + invalidityToString(dw.isInvalidWrapper()) + " - " + dw.getFileLocation());
+        }
+    }
+
+    private void listJvms(List<String> args) {
+        if (args.size() != 1) {
+            throw new RuntimeException(LISTJVMS + " do not expect argument");
+        }
+        for (VmInfo vm : manager.vmList) {
+            System.out.println(vm.getVmPid() + " " + vm.getVmName());
+        }
+    }
+
     private void printHelp() {
         System.out.println("Allowed are: " + LISTJVMS + " , " + LISTPLUGINS + " , " + LISTCLASSES + ", " + BASE64 + " , " + BYTES + ", " + DECOMPILE + ", " + VERBOSE);
-        System.out.println(VERBOSE + " will set this app to print out all Exceptions and some debugging strings. Then continues " );
-        System.out.println(HELP + "/" + H + " print this help end exit" );
-        System.out.println(LISTJVMS + " no arg expected, list available localhost JVMs " );
+        System.out.println(VERBOSE + " will set this app to print out all Exceptions and some debugging strings. Then continues ");
+        System.out.println(HELP + "/" + H + " print this help end exit");
+        System.out.println(LISTJVMS + " no arg expected, list available localhost JVMs ");
         System.out.println(LISTPLUGINS + "  no arg expected, currnently configured plugins with theirs status");
-        System.out.println(LISTCLASSES + "  one arg - pid or url of JVM. List its laoded classes." );
-        System.out.println(BYTES + "  two args - pid or url of JVM and  class to obtain - will stdou out its binary form" );
-        System.out.println(BASE64 + "  two args - pid or url of JVM and  class to obtain - will stdou out its binary encoded as base64" );
-        System.out.println(DECOMPILE+ "  three args - pid or url of JVM and  class to obtain and name/file of decompiler config - will stdou out decompiled class" );
+        System.out.println(LISTCLASSES + "  one arg - pid or url of JVM. List its laoded classes.");
+        System.out.println(BYTES + "  two args - pid or url of JVM and  class to obtain - will stdou out its binary form");
+        System.out.println(BASE64 + "  two args - pid or url of JVM and  class to obtain - will stdou out its binary encoded as base64");
+        System.out.println(DECOMPILE + "  three args - pid or url of JVM and  class to obtain and name/file of decompiler config - will stdou out decompiled class");
         System.out.println("Allowed are: " + LISTJVMS + " , " + LISTPLUGINS + " , " + LISTCLASSES + ", " + BASE64 + " , " + BYTES + ", " + DECOMPILE + ", " + VERBOSE);
     }
 
