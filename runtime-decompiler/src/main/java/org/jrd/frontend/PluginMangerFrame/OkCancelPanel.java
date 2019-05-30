f2d7c11139f9e08a41b2b1b52d01f917d3b39396
Vojtěch Kudláček <vkudlacek.vojtech@gmail.com>
2019-05-30 09:36:01 +0200

    Added plugin validation by compiling

--- runtime-decompiler/src/main/java/org/jrd/frontend/PluginMangerFrame/OkCancelPanel.java
+++ runtime-decompiler/src/main/java/org/jrd/frontend/PluginMangerFrame/OkCancelPanel.java
@@ -5,12 +5,13 @@ import javax.swing.border.MatteBorder;
 import java.awt.*;
 
 /**
- * Panel with two buttons "OK" and "Cancel"
+ * Panel with three buttons "Validate", "OK" and "Cancel"
  */
 public class OkCancelPanel extends JPanel{
 
     private final JButton okButton;
     private final JButton cancelButton;
+    private final JButton validateButton;
 
     OkCancelPanel(){
         this.setLayout(new GridBagLayout());
@@ -22,6 +23,8 @@ public class OkCancelPanel extends JPanel{
         okButton.setPreferredSize(new Dimension(90, 28));
         cancelButton = new JButton("Cancel");
         cancelButton.setPreferredSize(new Dimension(90, 28));
+        validateButton = new JButton("Validate");
+        validateButton.setPreferredSize(new Dimension(90,28));
 
         gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
@@ -30,15 +33,22 @@ public class OkCancelPanel extends JPanel{
         this.add(Box.createHorizontalGlue(), gbc);
         gbc.weightx = 0;
         gbc.gridx = 1;
-        this.add(okButton, gbc);
+        this.add(validateButton,gbc);
         gbc.gridx = 2;
         this.add(Box.createHorizontalStrut(15), gbc);
         gbc.gridx = 3;
-        this.add(cancelButton, gbc);
+        this.add(okButton, gbc);
         gbc.gridx = 4;
+        this.add(Box.createHorizontalStrut(15), gbc);
+        gbc.gridx = 5;
+        this.add(cancelButton, gbc);
+        gbc.gridx = 6;
         this.add(Box.createHorizontalStrut(20), gbc);
     }
 
+    public JButton getValidateButton(){
+        return validateButton;
+    }
     public JButton getOkButton() {
         return okButton;
     }
