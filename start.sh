560b1d2db9fdccadad9fed71f9433425ef80521f
Vojtěch Kudláček <vkudlacek.vojtech@gmail.com>
2019-06-04 13:42:03 +0200

    Renamed $path to $javac_home and changed /.. to dirname

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Locate JDK from $PATH
-path=`which javac`
-path=`readlink -f $path`
-path="$(dirname "$path")"
-path="$path/.."
+javac_home=`which javac`
+javac_home=`readlink -f $javac_home`
+javac_home="$(dirname "$(dirname "$javac_home")")"
 
-TOOLS=$path/lib/tools.jar
+TOOLS=$javac_home/lib/tools.jar
 RSYNTAXTEXTAREA=$(echo $HOME/.m2/repository/com/fifesoft/rsyntaxtextarea/*/rsyntaxtextarea-*.jar)
 GSON=$(echo $HOME/.m2/repository/com/google/code/gson/gson/*/gson-*.jar)
 BYTEMAN=$(echo $HOME/.m2/repository/org/jboss/byteman/byteman-install/*/byteman-install-*.jar)
 JRD=$(echo $HOME/.m2/repository/java-runtime-decompiler/runtime-decompiler/*/runtime-decompiler-*.jar)
 # launch application
-$path/bin/java -cp $TOOLS:\
+$javac_home/bin/java -cp $TOOLS:\
 $RSYNTAXTEXTAREA:\
 $GSON:\
 $BYTEMAN:\
