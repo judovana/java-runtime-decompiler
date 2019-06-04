73617affe3b65b0b86a4a874c0e798d24fa3f4d0
Vojtěch Kudláček <vkudlacek.vojtech@gmail.com>
2019-06-04 12:48:24 +0200

    Repaired start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 # Locate JDK from $PATH
-path=`which java`
-# go through symlinks until we are at /usr/lib/jvm/...
-while [[ ${path} != /usr/lib/jvm/* ]]
-do
-path=`readlink $path`
-done
-while [[ $(dirname "$path") != /usr/lib/jvm ]]
-do
+path=`which javac`
+path=`readlink -f $path`
 path="$(dirname "$path")"
-done
-export JAVA_HOME=$path
+path="$path/.."
 
+TOOLS=$path/lib/tools.jar
+RSYNTAXTEXTAREA=$(echo $HOME/.m2/repository/com/fifesoft/rsyntaxtextarea/*/rsyntaxtextarea-*.jar)
+GSON=$(echo $HOME/.m2/repository/com/google/code/gson/gson/*/gson-*.jar)
+BYTEMAN=$(echo $HOME/.m2/repository/org/jboss/byteman/byteman-install/*/byteman-install-*.jar)
+JRD=$(echo $HOME/.m2/repository/java-runtime-decompiler/runtime-decompiler/*/runtime-decompiler-*.jar)
 # launch application
-java -cp ${JAVA_HOME}/lib/tools.jar:\
-$HOME/.m2/repository/com/fifesoft/rsyntaxtextarea/2.6.1/rsyntaxtextarea-2.6.1.jar:\
-$HOME/.m2/repository/io/github/soc/directories/10/directories-10.jar:\
-$HOME/.m2/repository/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar:\
-$HOME/.m2/repository/java/runtime-decompiler/1.0.0-SNAPSHOT/runtime-decompiler-1.0.0-SNAPSHOT.jar\
+$path/bin/java -cp $TOOLS:\
+$RSYNTAXTEXTAREA:\
+$GSON:\
+$BYTEMAN:\
+$JRD\
  org.jrd.backend.data.Main
