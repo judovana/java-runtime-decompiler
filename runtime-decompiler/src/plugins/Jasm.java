8f2e9291565eb05e4b470d69ea4bc63dd60060c4
Jiri <jvanek@redhat.com>
2020-12-30 20:00:49 +0100

    Jasm dissasembler plugin made to work

--- runtime-decompiler/src/plugins/Jasm.java
+++ runtime-decompiler/src/plugins/Jasm.java
@@ -1,8 +1,32 @@
+import org.openjdk.asmtools.jdis.Main;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class Jasm {
 
     public String decompile(byte[] bytecode, String[] options) {
-		org.openjdk.asmtools.Main.main(new String[]{});
-        return "jasm invoked";
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final String utf8 = StandardCharsets.UTF_8.name();
+        try {
+            File file = File.createTempFile("jrd-jasm", "tmp.class");
+            file.deleteOnExit();
+            Files.write(file.toPath(), bytecode);
+            try (PrintStream ps = new PrintStream(baos, true, utf8)) {
+                Main jdis = new Main(ps, "jdis");
+                jdis.disasm(new String[]{file.getAbsolutePath()});
+            }
+            String data = baos.toString(utf8);
+            if (data.isEmpty()) {
+                return "No output, maybe inner class?";
+            }
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
     }
 }
