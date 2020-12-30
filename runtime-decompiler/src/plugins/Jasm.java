4efe8101d9f073e856d1358407436c11e7e3c7c8
Jiri <jvanek@redhat.com>
2020-12-30 20:48:28 +0100

    Added experimetnal compile method for jasm

--- runtime-decompiler/src/plugins/Jasm.java
+++ runtime-decompiler/src/plugins/Jasm.java
@@ -1,10 +1,11 @@
-import org.openjdk.asmtools.jdis.Main;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Jasm {
 
@@ -12,11 +13,11 @@ public class Jasm {
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
         final String utf8 = StandardCharsets.UTF_8.name();
         try {
-            File file = File.createTempFile("jrd-jasm", "tmp.class");
+            File file = File.createTempFile("jrd-jasm", "tmp.java");
             file.deleteOnExit();
             Files.write(file.toPath(), bytecode);
             try (PrintStream ps = new PrintStream(baos, true, utf8)) {
-                Main jdis = new Main(ps, "jdis");
+                org.openjdk.asmtools.jdis.Main jdis = new org.openjdk.asmtools.jdis.Main(ps, "jdis");
                 jdis.disasm(new String[]{file.getAbsolutePath()});
             }
             String data = baos.toString(utf8);
@@ -28,5 +29,24 @@ public class Jasm {
             e.printStackTrace();
             return e.toString();
         }
+    }
+
+    public Map<String,byte[]> compile(Map<String,String> src, String[] options) throws Exception {
+        File file = File.createTempFile("jrd-jasm", "tmp.dir");
+        file.delete();
+        file.mkdir();
+        file.deleteOnExit();
+        org.openjdk.asmtools.jasm.Main jasm = new org.openjdk.asmtools.jasm.Main(System.out, "jdis");
+        jasm.compile(new String[]{"-g", "-d",file.getAbsolutePath()});
+        Map<String,byte[]> r = new HashMap();
+        Files.walk(file.toPath()).filter(Files::isRegularFile).forEach((k) -> {
+                try {
+                    String futureFullyQualifiedNiceName = k.toString();
+                    r.put(futureFullyQualifiedNiceName, Files.readAllBytes(k));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
+        return r;
     }
 }
