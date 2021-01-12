cd3d54dd0d77b0e0d23c9f48a10c5348c9d17f9f
Jiri Vanek <jvanek@redhat.com>
2021-01-12 13:17:22 +0100

    Added support forjars on classapth

--- runtime-decompiler/src/main/java/org/jrd/backend/communication/FsAgent.java
+++ runtime-decompiler/src/main/java/org/jrd/backend/communication/FsAgent.java
@@ -3,8 +3,12 @@ package org.jrd.backend.communication;
 
 import org.jrd.backend.core.OutputController;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
@@ -12,8 +16,12 @@ import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 /**
  * This class is doing agent-like based operations on filesystem
@@ -64,12 +72,31 @@ public class FsAgent implements JrdAgent {
 
     private String sendByteCodeImpl(String clazz) throws IOException {
         for (File c : cp) {
-            String root = sanitize(c.getAbsolutePath());
-            String classOnFs = clazz.replace(".", File.separator) + ".class";
-            File f = new File(root + File.separator + classOnFs);
-            if (f.exists()) {
-                byte[] bytes = Files.readAllBytes(f.toPath());
-                return Base64.getEncoder().encodeToString(bytes);
+            if (c.isDirectory()) {
+                String root = sanitize(c.getAbsolutePath());
+                String classOnFs = clazz.replace(".", File.separator) + ".class";
+                File f = new File(root + File.separator + classOnFs);
+                if (f.exists()) {
+                    byte[] bytes = Files.readAllBytes(f.toPath());
+                    return Base64.getEncoder().encodeToString(bytes);
+                }
+            } else {
+                ZipFile zipFile = new ZipFile(c);
+                Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+                while (entries.hasMoreElements()) {
+                    ZipEntry ze = entries.nextElement();
+                    if (!ze.isDirectory()) {
+                        String clazzInJar = toClass((ze.getName()));
+                        if (clazzInJar.equals(clazz)) {
+                            byte[] data = new byte[(int) ze.getSize()];
+                            try (DataInputStream dis = new DataInputStream(zipFile.getInputStream(ze))) {
+                                dis.readFully(data);
+                            }
+                            return Base64.getEncoder().encodeToString(data);
+                        }
+                    }
+                }
             }
         }
         throw new IOException(clazz + " not found on CP");
@@ -89,10 +116,7 @@ public class FsAgent implements JrdAgent {
                     @Override
                     public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
                         String s = sanitize(path.toFile().getAbsolutePath());
-                        if (s.endsWith(".class")) {
-                            classes.add(toClass(s.substring(root.length() + 1)));
-                        } else {
-                        }
+                        addJustClass(s, classes, root);
                         return FileVisitResult.CONTINUE;
                     }
 
@@ -107,10 +131,26 @@ public class FsAgent implements JrdAgent {
                     }
                 });
             } else {
-                //is it jar/zip? trasnaprently traverse it!
+                ZipFile zipFile = new ZipFile(c);
+                Enumeration<? extends ZipEntry> entries = zipFile.entries();
+                while (entries.hasMoreElements()) {
+                    ZipEntry ze = entries.nextElement();
+                    //cache this? Se the read...
+                    if (!ze.isDirectory()) {
+                        addJustClass("/" + ze.getName(), classes, "");
+                    }
+                }
             }
         }
         return classes.stream().collect(Collectors.joining(";"));
+    }
+
+    private void addJustClass(String s, List<String> classes, String root) {
+        if (s.endsWith(".class")) {
+            classes.add(toClass(s.substring(root.length() + 1)));
+        } else {
+            OutputController.getLogger().log(OutputController.Level.MESSAGE_DEBUG, "ignored non .class element on cp: " + s);
+        }
     }
 
     private String toClass(String s) {
