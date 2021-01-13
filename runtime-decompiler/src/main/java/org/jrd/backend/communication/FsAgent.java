dfb4e3364f1820b984eaa17a1e1184fd078d9688
Jiri <jvanek@redhat.com>
2021-01-13 10:18:39 +0100

    Implemented overriding of classes on FS, including jars
    removed duplicated code in cp traversing

--- runtime-decompiler/src/main/java/org/jrd/backend/communication/FsAgent.java
+++ runtime-decompiler/src/main/java/org/jrd/backend/communication/FsAgent.java
@@ -3,12 +3,11 @@ package org.jrd.backend.communication;
 
 import org.jrd.backend.core.OutputController;
 
-import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
@@ -21,7 +20,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 
 /**
  * This class is doing agent-like based operations on filesystem
@@ -50,6 +48,9 @@ public class FsAgent implements JrdAgent {
                     return readClasses();
                 case "BYTES":
                     return sendByteCode(request);
+                case "OVERWRITE":
+                    uploadByteCode(request);
+                    return "OK";
                 case "HALT":
                     return "OK";
                 default:
@@ -61,91 +62,87 @@ public class FsAgent implements JrdAgent {
         }
     }
 
-    private String sendByteCode(String request) {
+    private Void uploadByteCode(String request) {
         String[] clazz = request.split("\\s+");
         try {
-            return sendByteCodeImpl(clazz[1]);
+            return new OperateOnCp<Void>(cp).operatetOnCp(clazz[1], new WriteingCpOperator(clazz[2]));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private String sendByteCodeImpl(String clazz) throws IOException {
-        for (File c : cp) {
-            if (c.isDirectory()) {
-                String root = sanitize(c.getAbsolutePath());
-                String classOnFs = clazz.replace(".", File.separator) + ".class";
-                File f = new File(root + File.separator + classOnFs);
-                if (f.exists()) {
-                    byte[] bytes = Files.readAllBytes(f.toPath());
-                    return Base64.getEncoder().encodeToString(bytes);
-                }
-            } else {
-                ZipFile zipFile = new ZipFile(c);
-                Enumeration<? extends ZipEntry> entries = zipFile.entries();
-
-                while (entries.hasMoreElements()) {
-                    ZipEntry ze = entries.nextElement();
-                    if (!ze.isDirectory()) {
-                        String clazzInJar = toClass((ze.getName()));
-                        if (clazzInJar.equals(clazz)) {
-                            byte[] data = new byte[(int) ze.getSize()];
-                            try (DataInputStream dis = new DataInputStream(zipFile.getInputStream(ze))) {
-                                dis.readFully(data);
-                            }
-                            return Base64.getEncoder().encodeToString(data);
-                        }
-                    }
-                }
-            }
+    private String sendByteCode(String request) {
+        String[] clazz = request.split("\\s+");
+        try {
+            String s = new OperateOnCp<String>(cp).operatetOnCp(clazz[1], new ReadingCpOperator());
+            return s;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        throw new IOException(clazz + " not found on CP");
     }
 
     private String readClasses() throws IOException {
         List<String> classes = new ArrayList<>();
-        for (File c : cp) {
-            String root = sanitize(c.getAbsolutePath());
-            if (c.isDirectory()) {
-                Files.walkFileTree(c.toPath(), new FileVisitor<Path>() {
-                    @Override
-                    public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
-                        String s = sanitize(path.toFile().getAbsolutePath());
-                        addJustClass(s, classes, root);
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult visitFileFailed(Path path, IOException e) throws IOException {
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult postVisitDirectory(Path path, IOException e) throws IOException {
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-            } else {
-                ZipFile zipFile = new ZipFile(c);
-                Enumeration<? extends ZipEntry> entries = zipFile.entries();
-                while (entries.hasMoreElements()) {
-                    ZipEntry ze = entries.nextElement();
-                    //cache this? Se the read...
-                    if (!ze.isDirectory()) {
-                        addJustClass("/" + ze.getName(), classes, "");
-                    }
-                }
-            }
-        }
+        new OperateOnCp<Void>(cp).operatetOnCp(null, new ListingCpOperator(classes));
         return classes.stream().collect(Collectors.joining(";"));
     }
 
-    private void addJustClass(String s, List<String> classes, String root) {
+    private interface CpOperator<T> {
+        T onDirEntry(File dir, File clazz, String fqn) throws IOException;
+
+        T onJarEntry(File file, ZipFile zipFile, ZipEntry ze, String fqn) throws IOException;
+
+        T finalizirung();
+    }
+
+    private static class OperateOnCp<T> {
+        private final List<File> cp;
+
+        public OperateOnCp(List<File> cp) {
+            this.cp = cp;
+        }
+
+        private T operatetOnCp(String clazz, CpOperator<T> op) throws IOException {
+            for (File c : cp) {
+                if (c.isDirectory()) {
+                    String root = sanitize(c.getAbsolutePath());
+                    if (clazz == null) {
+                        //no return, search
+                        op.onDirEntry(c, null, null);
+                    } else {
+                        String classOnFs = clazz.replace(".", File.separator) + ".class";
+                        File f = new File(root + File.separator + classOnFs);
+                        if (f.exists()) {
+                            return op.onDirEntry(c, f, clazz);
+                        }
+                    }
+                } else {
+                    ZipFile zipFile = new ZipFile(c);
+                    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+                    while (entries.hasMoreElements()) {
+                        ZipEntry ze = entries.nextElement();
+                        if (!ze.isDirectory()) {
+                            String clazzInJar = toClass((ze.getName()));
+                            if (clazz == null) {
+                                //no return reading all
+                                op.onJarEntry(c, zipFile, ze, clazz);
+                            } else {
+                                if (clazzInJar.equals(clazz)) {
+                                    return op.onJarEntry(c, zipFile, ze, clazz);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (clazz == null) {
+                return op.finalizirung();
+            }
+            throw new IOException(clazz + " not found on CP");
+        }
+    }
+
+    private static void addJustClass(String s, List<String> classes, String root) {
         if (s.endsWith(".class")) {
             classes.add(toClass(s.substring(root.length() + 1)));
         } else {
@@ -153,11 +150,11 @@ public class FsAgent implements JrdAgent {
         }
     }
 
-    private String toClass(String s) {
+    private static String toClass(String s) {
         return s.replace("/", ".").replaceAll("\\\\", ".").replaceAll(".class$", "");
     }
 
-    private String sanitize(String s) {
+    private static String sanitize(String s) {
         while (s.contains("//") || s.contains("\\\\")) {
             s = s.replaceAll("//", "/");
             s = s.replaceAll("\\\\\\\\", "\\");
@@ -166,5 +163,105 @@ public class FsAgent implements JrdAgent {
             s = s.substring(0, s.length() - 2);
         }
         return s;
+    }
+
+    private static class WriteingCpOperator implements CpOperator<Void> {
+        private final String body;
+
+        public WriteingCpOperator(String body) {
+            this.body = body;
+        }
+
+        @Override
+        public Void onDirEntry(File dir, File clazz, String fqn) throws IOException {
+            Files.write(clazz.toPath(), Base64.getDecoder().decode(body));
+            OutputController.getLogger().log(OutputController.Level.MESSAGE_DEBUG, "written " + clazz.getAbsolutePath());
+            return null;
+        }
+
+        @Override
+        public Void onJarEntry(File file, ZipFile zipFile, ZipEntry ze, String fqn) throws IOException {
+            try (FileSystem fs = FileSystems.newFileSystem(file.toPath(), null)) {
+                Path fileInsideZipPath = fs.getPath(ze.getName());
+                Files.write(fileInsideZipPath, Base64.getDecoder().decode(body));
+                OutputController.getLogger().log(OutputController.Level.MESSAGE_DEBUG, "written " + file.getAbsolutePath() + "!" + fileInsideZipPath);
+            }
+            return null;
+        }
+
+        @Override
+        public Void finalizirung() {
+            return null;
+        }
+    }
+
+    private static class ReadingCpOperator implements CpOperator<String> {
+        @Override
+        public String onDirEntry(File dir, File clazz, String fqn) throws IOException {
+            byte[] bytes = Files.readAllBytes(clazz.toPath());
+            return Base64.getEncoder().encodeToString(bytes);
+        }
+
+        @Override
+        public String onJarEntry(File file, ZipFile zipFile, ZipEntry ze, String fqn) throws IOException {
+            byte[] data = new byte[(int) ze.getSize()];
+            try (DataInputStream dis = new DataInputStream(zipFile.getInputStream(ze))) {
+                dis.readFully(data);
+            }
+            return Base64.getEncoder().encodeToString(data);
+        }
+
+        @Override
+        public String finalizirung() {
+            return null;
+        }
+    }
+
+    private static class ListingCpOperator implements CpOperator<Void> {
+        private final List<String> classes;
+
+        public ListingCpOperator(List<String> classes) {
+            this.classes = classes;
+        }
+
+        @Override
+        public Void onDirEntry(File c, File clazz, String fqn) throws IOException {
+            Files.walkFileTree(c.toPath(), new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+                    String s = sanitize(path.toFile().getAbsolutePath());
+                    String root = sanitize(c.getAbsolutePath());
+                    addJustClass(s, classes, root);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path path, IOException e) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path path, IOException e) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            return null;
+        }
+
+        @Override
+        public Void onJarEntry(File file, ZipFile zipFile, ZipEntry ze, String fqn) throws IOException {
+            addJustClass("/" + ze.getName(), classes, "");
+            return null;
+        }
+
+        @Override
+        public Void finalizirung() {
+            return null;
+        }
     }
 }
