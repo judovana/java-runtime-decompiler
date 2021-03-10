94d6a58605ebfd5bf6c5cdcf39b5e1364359c800
Sotirios Pupakis <sotirios.pupakis@gmail.com>
2021-03-10 13:13:19 +0100

    Test now throw IOException and don't have try-catches

--- runtime-decompiler/src/test/java/org/jrd/frontend/HexSearch/HexSearchTest.java
+++ runtime-decompiler/src/test/java/org/jrd/frontend/HexSearch/HexSearchTest.java
@@ -12,197 +12,172 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HexSearchTest {
     @Test
-    void hexSearchTestsString() {
+    void hexSearchTestsString() throws IOException {
         // Create test data
         byte[] testData = "test 123 456 789 tset +-*+// te te te 123 123 56 56".getBytes(StandardCharsets.UTF_8);
 
+        HexEditor hex = new HexEditor();
+        hex.open(new ByteArrayInputStream(testData));
+        HexSearch hexSearchEngine = new HexSearch(hex);
+
+        HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.TEXT;
+        String testString = "test";
+
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 0);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 4);
         try {
-            HexEditor hex = new HexEditor();
-            hex.open(new ByteArrayInputStream(testData));
-            HexSearch hexSearchEngine = new HexSearch(hex);
-
-            HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.TEXT;
-            String testString = "test";
-
+            testString = "";
             hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 0);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 4);
-            try {
-                testString = "";
-                hexSearchEngine.searchHexCode(testString, type);
-                fail("Did not throw exception on empty TEXT");
-            } catch (Exception e) {
-                // Good
-            }
-            assertFalse(hexSearchEngine.getSearchState().isFound());
-
-            testString = "te te";
-            hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 29);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 34);
-        } catch (IOException ex) {
-            // Fail in case of an exception
-            fail("hex.open() failed");
+            fail("Did not throw exception on empty TEXT");
+        } catch (Exception e) {
+            // Good
         }
+        assertFalse(hexSearchEngine.getSearchState().isFound());
+
+        testString = "te te";
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 29);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 34);
     }
 
     @Test
-    void hexSearchTestsHex() {
+    void hexSearchTestsHex() throws IOException {
+        // Create test data
+        byte[] testData = "test 123 456 789 tset +-*+// te te te 123 123 56 56".getBytes(StandardCharsets.UTF_8);
+        
+        HexEditor hex = new HexEditor();
+        hex.open(new ByteArrayInputStream(testData));
+        HexSearch hexSearchEngine = new HexSearch(hex);
+
+        HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.HEX;
+        String testString = "74 65 73 74";
+
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 0);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 4);
+        try {
+            testString = "";
+            hexSearchEngine.searchHexCode(testString, type);
+            fail("Did not throw exception on empty HEX");
+        } catch (Exception e) {
+            // Good
+        }
+        assertFalse(hexSearchEngine.getSearchState().isFound());
+
+        testString = "74 65 20 74 65";
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 29);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 34);
+    }
+
+    @Test
+    void hexSearchTestsInt() throws IOException {
         // Create test data
         byte[] testData = "test 123 456 789 tset +-*+// te te te 123 123 56 56".getBytes(StandardCharsets.UTF_8);
 
+        HexEditor hex = new HexEditor();
+        hex.open(new ByteArrayInputStream(testData));
+        HexSearch hexSearchEngine = new HexSearch(hex);
+
+        HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.INT;
+        String testString = "116 101 115 116";
+
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 0);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 4);
         try {
-            HexEditor hex = new HexEditor();
-            hex.open(new ByteArrayInputStream(testData));
-            HexSearch hexSearchEngine = new HexSearch(hex);
-
-            HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.HEX;
-            String testString = "74 65 73 74";
-
+            testString = "";
             hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 0);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 4);
-            try {
-                testString = "";
-                hexSearchEngine.searchHexCode(testString, type);
-                fail("Did not throw exception on empty HEX");
-            } catch (Exception e) {
-                // Good
-            }
-            assertFalse(hexSearchEngine.getSearchState().isFound());
-
-            testString = "74 65 20 74 65";
-            hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 29);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 34);
-        } catch (IOException ex) {
-            // Fail in case of an exception
-            fail("hex.open() failed");
+            fail("Did not throw exception on empty INT");
+        } catch (Exception e) {
+            // Good
         }
+        assertFalse(hexSearchEngine.getSearchState().isFound());
+
+        testString = "116 101 32 116 101";
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 29);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 34);
     }
 
     @Test
-    void hexSearchTestsInt() {
-        // Create test data
-        byte[] testData = "test 123 456 789 tset +-*+// te te te 123 123 56 56".getBytes(StandardCharsets.UTF_8);
-
-        try {
-            HexEditor hex = new HexEditor();
-            hex.open(new ByteArrayInputStream(testData));
-            HexSearch hexSearchEngine = new HexSearch(hex);
-
-            HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.INT;
-            String testString = "116 101 115 116";
-
-            hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 0);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 4);
-            try {
-                testString = "";
-                hexSearchEngine.searchHexCode(testString, type);
-                fail("Did not throw exception on empty INT");
-            } catch (Exception e) {
-                // Good
-            }
-            assertFalse(hexSearchEngine.getSearchState().isFound());
-
-            testString = "116 101 32 116 101";
-            hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 29);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 34);
-        } catch (IOException ex) {
-            // Fail in case of an exception
-            fail("hex.open() failed");
-        }
-    }
-
-    @Test
-    void hexSearchTestNext() {
+    void hexSearchTestNext() throws IOException {
         // Create test data
         byte[] testData = "test 123 456 789 tset/ te te te 123 123 56 56".getBytes(StandardCharsets.UTF_8);
 
-        try {
-            HexEditor hex = new HexEditor();
-            hex.open(new ByteArrayInputStream(testData));
-            HexSearch hexSearchEngine = new HexSearch(hex);
+        HexEditor hex = new HexEditor();
+        hex.open(new ByteArrayInputStream(testData));
+        HexSearch hexSearchEngine = new HexSearch(hex);
 
-            HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.TEXT;
-            String testString = "123";
+        HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.TEXT;
+        String testString = "123";
 
-            hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
 
-            hexSearchEngine.next(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 32);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 35);
+        hexSearchEngine.next(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 32);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 35);
 
-            hexSearchEngine.next(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 36);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 39);
+        hexSearchEngine.next(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 36);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 39);
 
-            hexSearchEngine.next(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 36);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 39);
-        } catch (IOException ex) {
-            // Fail in case of an exception
-            fail("hex.open() failed");
-        }
+        hexSearchEngine.next(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 36);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 39);
     }
 
     @Test
-    void hexSearchTestPrev() {
+    void hexSearchTestPrev() throws IOException {
         // Create test data
         byte[] testData = "test 123 456 789 tset/ te te te 123 123 56 56".getBytes(StandardCharsets.UTF_8);
 
-        try {
-            HexEditor hex = new HexEditor();
-            hex.open(new ByteArrayInputStream(testData));
-            HexSearch hexSearchEngine = new HexSearch(hex);
+        HexEditor hex = new HexEditor();
+        hex.open(new ByteArrayInputStream(testData));
+        HexSearch hexSearchEngine = new HexSearch(hex);
 
-            HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.TEXT;
-            String testString = "123";
+        HexSearch.HexSearchOptions type = HexSearch.HexSearchOptions.TEXT;
+        String testString = "123";
 
-            hexSearchEngine.searchHexCode(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
+        hexSearchEngine.searchHexCode(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
 
-            hexSearchEngine.next(testString, type);
-            hexSearchEngine.next(testString, type);
-            hexSearchEngine.next(testString, type);
+        hexSearchEngine.next(testString, type);
+        hexSearchEngine.next(testString, type);
+        hexSearchEngine.next(testString, type);
 
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 36);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 39);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 36);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 39);
 
-            hexSearchEngine.previous(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 32);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 35);
+        hexSearchEngine.previous(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 32);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 35);
 
-            hexSearchEngine.previous(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
+        hexSearchEngine.previous(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
 
-            hexSearchEngine.previous(testString, type);
-            assertTrue(hexSearchEngine.getSearchState().isFound());
-            assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
-            assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
-        } catch (IOException ex) {
-            // Fail in case of an exception
-            fail("hex.open() failed");
-        }
+        hexSearchEngine.previous(testString, type);
+        assertTrue(hexSearchEngine.getSearchState().isFound());
+        assertEquals(hexSearchEngine.getSearchState().getStart(), 5);
+        assertEquals(hexSearchEngine.getSearchState().getEnd(), 8);
     }
 }
