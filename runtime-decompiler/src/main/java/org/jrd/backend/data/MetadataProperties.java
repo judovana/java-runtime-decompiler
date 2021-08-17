f298f8c5e02cae79bb67f99e8bbe8e539c2fa5a5
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-08-17 16:52:06 +0200

    Replaced manifest-based metadata propagation with property file and class The main benefit being functionality even when building in debug mode.

---
+++ runtime-decompiler/src/main/java/org/jrd/backend/data/MetadataProperties.java
@@ -0,0 +1,50 @@
+package org.jrd.backend.data;
+
+import org.jrd.backend.core.OutputController;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class MetadataProperties {
+    private final Properties properties;
+
+    private static final String GROUP_ID_KEY = "groupId";
+    private static final String VERSION_KEY = "version";
+    private static final String TIMESTAMP_KEY = "timestamp";
+
+    private static class PropertiesHolder {
+        private static final MetadataProperties INSTANCE = new MetadataProperties();
+    }
+
+    private MetadataProperties() {
+        properties = new java.util.Properties();
+
+        try (InputStream stream = getClass().getResourceAsStream("/version.prop")) {
+            properties.load(stream);
+        } catch (IOException e) {
+            OutputController.getLogger().log(OutputController.Level.MESSAGE_ALL, "Unable to read property file.");
+        }
+    }
+
+    public static MetadataProperties getInstance() {
+        return PropertiesHolder.INSTANCE;
+    }
+
+    public String getGroup() {
+        return properties.getProperty(GROUP_ID_KEY, "group.notfound");
+    }
+
+    public String getVersion() {
+        return properties.getProperty(VERSION_KEY, "version.notfound");
+    }
+
+    public String getTimestamp() {
+        return properties.getProperty(TIMESTAMP_KEY, "build.timestamp.notfound");
+    }
+
+    @Override
+    public String toString() {
+        return String.join(" - ", getGroup(), "JRD", getVersion(), getTimestamp());
+    }
+}
--- runtime-decompiler/src/main/java/org/jrd/frontend/AboutFrame/AboutView.java
+++ runtime-decompiler/src/main/java/org/jrd/frontend/AboutFrame/AboutView.java
@@ -1,15 +1,14 @@
 package org.jrd.frontend.AboutFrame;
 
 import org.jrd.backend.core.OutputController;
+import org.jrd.backend.data.MetadataProperties;
 import org.jrd.frontend.MainFrame.MainFrameView;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import java.awt.*;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.util.Properties;
 
 public class AboutView extends JDialog {
 
@@ -23,7 +22,7 @@ public class AboutView extends JDialog {
 
         JEditorPane editorPane = new JEditorPane("text/html", "<html><body style=\"" + style + "\">" +
             "<h2>Java-Runtime-Decompiler</h2>" +
-            "Version " + getVersion() + "<br />" +
+            "Version " + MetadataProperties.getInstance().getVersion() + "<br />" +
             "Licenced under the GNU General Public License v3.0<br />" +
             "Visit <a href=\"https://github.com/pmikova/java-runtime-decompiler\">the GitHub repository</a> for more information.<br />" +
             "</body></html>");
@@ -44,21 +43,5 @@ public class AboutView extends JDialog {
         editorPane.setBackground(new Color(label.getBackground().getRGB()));
 
         JOptionPane.showMessageDialog(mainFrameView.getMainFrame(), editorPane, "About Java-Runtime-Decompiler", JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    private String getVersion(){
-        String path = "/version.prop";
-        InputStream stream = getClass().getResourceAsStream(path);
-        if(stream == null) {
-            return "UNKNOWN";
-        }
-        Properties props = new Properties();
-        try {
-            props.load(stream);
-            stream.close();
-            return (String) props.get("version");
-        } catch (IOException e) {
-            return "UNKNOWN";
-        }
     }
 }
