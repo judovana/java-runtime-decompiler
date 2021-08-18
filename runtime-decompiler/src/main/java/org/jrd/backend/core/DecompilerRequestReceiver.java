d556c644959d455a4fea7a2b4d72ce6fc8a1590d
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-08-18 12:35:53 +0200

    Extracted comparator of classes list into inner class The length of the custom comparisons was too distracting to be left amidst normal code.

--- runtime-decompiler/src/main/java/org/jrd/backend/core/DecompilerRequestReceiver.java
+++ runtime-decompiler/src/main/java/org/jrd/backend/core/DecompilerRequestReceiver.java
@@ -9,6 +9,7 @@ import org.jrd.backend.core.AgentRequestAction.RequestAction;
 import org.jrd.backend.data.VmInfo;
 import org.jrd.backend.data.VmManager;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -182,52 +183,16 @@ public class DecompilerRequestReceiver {
     private String getAllLoadedClassesAction(String hostname, int listenPort, String vmId, int vmPid) {
         try {
             ResponseWithPort reply = getResponse(hostname, listenPort, vmId, vmPid, "CLASSES");
-            String[] arrayOfClasses = parseClasses(reply.response);
-            if (arrayOfClasses != null) {
-                Arrays.sort(arrayOfClasses, new Comparator<String>() {
-                    @SuppressFBWarnings(
-                            value = "NP_NULL_ON_SOME_PATH_MIGHT_BE_INFEASIBLE",
-                            justification = "SpotBugs false reports possible NP dereference, even though both o1 & o2 are tested for nullness. "
-                    )
-                    @Override
-                    public int compare(String o1, String o2) {
-                        if (o1 == null && o2 == null) {
-                            return 0;
-                        }
-                        if (o1 == null && o2 != null) {
-                            return 1;
-                        }
-                        if (o1 != null && o2 == null) {
-                            return -1;
-                        }
-                        if (o1.startsWith("[") && !o2.startsWith("[")) {
-                            return 1;
-                        }
-                        if (!o1.startsWith("[") && o2.startsWith("[")) {
-                            return -1;
-                        }
-                        if (o1.contains(UNDECOMPILABLE_LAMBDA) && !o2.contains(UNDECOMPILABLE_LAMBDA)) {
-                            return 1;
-                        }
-                        if (!o1.contains(UNDECOMPILABLE_LAMBDA) && o2.contains(UNDECOMPILABLE_LAMBDA)) {
-                            return -1;
-                        }
-                        if (LAMBDA_FORM.matcher(o1).matches() && !LAMBDA_FORM.matcher(o2).matches()) {
-                            return 1;
-                        }
-                        if (!LAMBDA_FORM.matcher(o1).matches() && LAMBDA_FORM.matcher(o2).matches()) {
-                            return -1;
-                        }
 
-                        return o1.compareTo(o2);
-                    }
-                });
-            }
+            String[] arrayOfClasses = parseClasses(reply.response);
+            Arrays.sort(arrayOfClasses, new ClassesComparator());
+
             VmDecompilerStatus status = new VmDecompilerStatus();
             status.setHostname(hostname);
             status.setListenPort(reply.port);
             status.setVmId(vmId);
             status.setLoadedClassNames(arrayOfClasses);
+
             vmManager.getVmInfoByID(vmId).replaceVmDecompilerStatus(status);
         } catch (Exception ex) {
             OutputController.getLogger().log(OutputController.Level.MESSAGE_ALL, ex);
@@ -274,4 +239,43 @@ public class DecompilerRequestReceiver {
 
     }
 
+    private static class ClassesComparator implements Comparator<String>, Serializable {
+
+        @SuppressFBWarnings(
+                value = "NP_NULL_ON_SOME_PATH_MIGHT_BE_INFEASIBLE",
+                justification = "SpotBugs false reports possible NP dereference, even though both o1 & o2 are tested for nullness. "
+        )
+        @Override
+        public int compare(String o1, String o2) {
+            if (o1 == null && o2 == null) {
+                return 0;
+            }
+            if (o1 == null && o2 != null) {
+                return 1;
+            }
+            if (o1 != null && o2 == null) {
+                return -1;
+            }
+            if (o1.startsWith("[") && !o2.startsWith("[")) {
+                return 1;
+            }
+            if (!o1.startsWith("[") && o2.startsWith("[")) {
+                return -1;
+            }
+            if (o1.contains(UNDECOMPILABLE_LAMBDA) && !o2.contains(UNDECOMPILABLE_LAMBDA)) {
+                return 1;
+            }
+            if (!o1.contains(UNDECOMPILABLE_LAMBDA) && o2.contains(UNDECOMPILABLE_LAMBDA)) {
+                return -1;
+            }
+            if (LAMBDA_FORM.matcher(o1).matches() && !LAMBDA_FORM.matcher(o2).matches()) {
+                return 1;
+            }
+            if (!LAMBDA_FORM.matcher(o1).matches() && LAMBDA_FORM.matcher(o2).matches()) {
+                return -1;
+            }
+
+            return o1.compareTo(o2);
+        }
+    }
 }
