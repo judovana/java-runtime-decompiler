09dd397d907dd4a059171a291ca94cc6c8c9344b
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-08-21 18:17:01 +0200

    Extracted strings used in Config to static variables

--- runtime-decompiler/src/main/java/org/jrd/backend/data/Config.java
+++ runtime-decompiler/src/main/java/org/jrd/backend/data/Config.java
@@ -24,17 +24,17 @@ public class Config {
 
     private final Gson gson;
     private HashMap<String, String> configMap;
-    private String configFilePath;
-    private String legacyConfigFilePath;
+
+    private static final String CONFIG_FILE_PATH = Directories.getConfigDirectory() + File.separator + "config.json";
+    private static final String LEGACY_CONFIG_FILE_PATH = Directories.getConfigDirectory() + File.separator + "config.cfg";
+
+    private static final String AGENT_PATH_KEY = "AGENT_PATH";
 
     private static class ConfigHolder {
         private static final Config INSTANCE = new Config();
     }
 
     private Config() {
-        String parentDir = Directories.getConfigDirectory();
-        configFilePath = parentDir + File.separator + "config.json";
-        legacyConfigFilePath = parentDir + File.separator + "config.cfg";
         gson = new Gson();
 
         try {
@@ -49,11 +49,11 @@ public class Config {
     }
 
     public String getAgentRawPath() {
-        return ExpandableUrl.createFromPath(configMap.get("AGENT_PATH")).getRawPath();
+        return ExpandableUrl.createFromPath(configMap.get(AGENT_PATH_KEY)).getRawPath();
     }
 
     public String getAgentExpandedPath() {
-        String expandedPath = ExpandableUrl.createFromPath(configMap.get("AGENT_PATH")).getExpandedPath();
+        String expandedPath = ExpandableUrl.createFromPath(configMap.get(AGENT_PATH_KEY)).getExpandedPath();
         if(isOsWindows() && expandedPath.length() > 0 && expandedPath.charAt(0) == '/') { // Agent attaching fails on Windows when path starts with a slash
             expandedPath = expandedPath.substring(1);
         }
@@ -62,7 +62,7 @@ public class Config {
 
     public void setAgentPath(String agentPath) {
         if (agentPath.endsWith(".jar")) {
-            configMap.put("AGENT_PATH", ExpandableUrl.createFromPath(agentPath).getRawPath());
+            configMap.put(AGENT_PATH_KEY, ExpandableUrl.createFromPath(agentPath).getRawPath());
         } else {
             OutputController.getLogger().log(OutputController.Level.MESSAGE_ALL, new RuntimeException("Agent must be a .jar file"));
         }
@@ -70,15 +70,15 @@ public class Config {
 
     private void loadConfigFile() throws IOException {
         configMap = new HashMap<>();
-        configMap.put("AGENT_PATH", "");
-        File confFile = new File(configFilePath);
-        File legacyConfFile = new File(legacyConfigFilePath);
+        configMap.put(AGENT_PATH_KEY, "");
+        File confFile = new File(CONFIG_FILE_PATH);
+        File legacyConfFile = new File(LEGACY_CONFIG_FILE_PATH);
         if (confFile.exists()) {
             try (FileReader reader = new FileReader(confFile, StandardCharsets.UTF_8)){
                 configMap = gson.fromJson(reader, configMap.getClass());
             }
         }else if (legacyConfFile.exists()){
-            Files.readAllLines(Paths.get(legacyConfigFilePath)).forEach(s -> {
+            Files.readAllLines(Paths.get(LEGACY_CONFIG_FILE_PATH)).forEach(s -> {
                 String[] kv = s.split("===");
                 configMap.put(kv[0], kv[1]);
             });
@@ -88,7 +88,7 @@ public class Config {
     }
 
     public void saveConfigFile() throws IOException {
-        File confFile = new File(configFilePath);
+        File confFile = new File(CONFIG_FILE_PATH);
         if (!confFile.getParentFile().exists()){
             confFile.getParentFile().mkdirs();
         }
@@ -96,7 +96,7 @@ public class Config {
             confFile.createNewFile();
         }
 
-        Files.write(Paths.get(configFilePath), Collections.singleton(gson.toJson(configMap)), StandardCharsets.UTF_8);
+        Files.write(Paths.get(CONFIG_FILE_PATH), Collections.singleton(gson.toJson(configMap)), StandardCharsets.UTF_8);
     }
 
 }
