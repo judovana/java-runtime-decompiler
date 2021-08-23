a3fbad883f05ffd767476b3a5ad76eaea37942c9
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-08-23 16:56:14 +0200

    Made both bytecodeSearch and hexSearch text blink red if it was not found

--- runtime-decompiler/src/main/java/org/fife/ui/hex/event/HexSearchDocumentListener.java
+++ runtime-decompiler/src/main/java/org/fife/ui/hex/event/HexSearchDocumentListener.java
@@ -6,17 +6,24 @@ import org.jrd.backend.core.OutputController;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.awt.event.ActionListener;
 
 public class HexSearchDocumentListener implements DocumentListener {
 
     private final HexSearch hexSearchEngine;
     private final JTextField hexSearch;
     private final JComboBox<HexSearch.HexSearchOptions> hexSearchType;
+    private ActionListener wasNotFoundListener = null;
 
     public HexSearchDocumentListener(HexSearch hexSearchEngine, JTextField hexSearch, JComboBox<HexSearch.HexSearchOptions> hexSearchType) {
         this.hexSearchEngine = hexSearchEngine;
         this.hexSearch = hexSearch;
         this.hexSearchType = hexSearchType;
+    }
+
+    public HexSearchDocumentListener(HexSearch hexSearchEngine, JTextField hexSearch, JComboBox<HexSearch.HexSearchOptions> hexSearchType, ActionListener wasNotFoundListener) {
+        this(hexSearchEngine, hexSearch, hexSearchType);
+        this.wasNotFoundListener = wasNotFoundListener;
     }
 
     @Override
@@ -36,7 +43,11 @@ public class HexSearchDocumentListener implements DocumentListener {
 
     private void find() {
         try {
-            hexSearchEngine.searchHexCode(hexSearch.getText(), (HexSearch.HexSearchOptions) hexSearchType.getSelectedItem());
+            boolean wasFound = hexSearchEngine.searchHexCode(hexSearch.getText(), (HexSearch.HexSearchOptions) hexSearchType.getSelectedItem());
+
+            if (!wasFound && wasNotFoundListener != null) {
+                wasNotFoundListener.actionPerformed(null);
+            }
         } catch (Exception e) {
             OutputController.getLogger().log(e);
         }
