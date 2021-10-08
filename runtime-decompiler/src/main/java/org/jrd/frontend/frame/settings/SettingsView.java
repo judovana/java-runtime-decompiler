4d3ae5b6c66491fbdf46d28d1102c54862c92c0c
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-10-08 18:55:10 +0200

    Added frontend for setting compiler arguments

--- runtime-decompiler/src/main/java/org/jrd/frontend/frame/settings/SettingsView.java
+++ runtime-decompiler/src/main/java/org/jrd/frontend/frame/settings/SettingsView.java
@@ -100,6 +100,8 @@ public class SettingsView extends JDialog {
 
         private JLabel compilationSettingsLabel;
         private JCheckBox useHostSystemClassesCheckBox;
+        private JLabel compilerArgsLabel;
+        private JTextField compilerArgsTextField;
 
         public CompilationSettingsPanel(boolean initialUseHostSystemClasses) {
             compilationSettingsLabel = new JLabel("Compilation settings");
@@ -107,18 +109,34 @@ public class SettingsView extends JDialog {
                     "Use host system classes during compilation phase of class overwrite",
                     initialUseHostSystemClasses
             );
+            compilerArgsLabel = new JLabel("Compiler arguments");
+            compilerArgsTextField = new JTextField();
+            compilerArgsTextField.setToolTipText("Arguments that get passed to the compiler, eg. '-source 5 -target 8 -release 9 -Xlint'.");
 
             this.setLayout(new GridBagLayout());
             GridBagConstraints gbc = new GridBagConstraints();
             gbc.anchor = GridBagConstraints.NORTHWEST;
             gbc.fill = GridBagConstraints.BOTH;
             gbc.insets = new Insets(5, 5, 5, 5);
+            gbc.gridwidth = 2;
             gbc.weightx = 1; // required or else contents are centered
 
             this.add(compilationSettingsLabel, gbc);
 
             gbc.gridy = 1;
             this.add(useHostSystemClassesCheckBox, gbc);
+
+            gbc.insets = new Insets(5, 5 + useHostSystemClassesCheckBox.getInsets().left, 5, 5);
+            gbc.gridwidth = 1;
+            gbc.weightx = 0;
+            gbc.gridy = 2;
+            this.add(compilerArgsLabel, gbc);
+
+            gbc.insets = new Insets(5, 5, 5, 5);
+            gbc.weightx = 1;
+            gbc.weighty = 1;
+            gbc.gridx = 1;
+            this.add(compilerArgsTextField, gbc);
         }
 
         public boolean shouldUseHostSystemClassesCheckBox() {
