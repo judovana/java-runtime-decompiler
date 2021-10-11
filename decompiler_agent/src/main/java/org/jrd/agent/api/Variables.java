a161c6626e3d371f291f8794debb1a79c46411a6
Jiri Vanek <jvanek@redhat.com>
2021-10-11 18:06:49 +0200

    added agent api for local, instance based, variables

--- decompiler_agent/src/main/java/org/jrd/agent/api/Variables.java
+++ decompiler_agent/src/main/java/org/jrd/agent/api/Variables.java
@@ -14,6 +14,7 @@ public class Variables {
         NoSuchFakeVariableException.init();
         FakeVariableAlreadyDeclaredException.init();
         Global.init();
+        Local.init();
     }
 
     protected Variables() {
@@ -64,6 +65,7 @@ public class Variables {
         protected Global() {
         }
 
+        // todo, remove synchronisedMap, and create second, synchronised api
         private static final Map<String, Object> GLOBALS = Collections.synchronizedMap(new HashMap<>());
 
         public static void init() {
@@ -125,8 +127,101 @@ public class Variables {
             }
         }
 
-        public static void removeAll() throws NoSuchFakeVariableException {
+        public static void removeAll() {
             GLOBALS.clear();
+        }
+    }
+
+    public static class Local {
+
+        protected Local() {
+        }
+
+        // todo, remove synchronisedMap, and create second, synchronised api
+        private static final Map<Object, Map<String, Object>> LOCALS = Collections.synchronizedMap(new HashMap<>());
+
+        public static void init() {
+        }
+
+        private static synchronized Map<String, Object> getCreateIfNecessary(Object owner) {
+            Map<String, Object> thisOnes = LOCALS.get(owner);
+            if (thisOnes == null) {
+                thisOnes = new HashMap<>();
+                LOCALS.put(owner, thisOnes);
+            }
+            return thisOnes;
+        }
+
+        public static Object set(Object owner, String name, Object value) {
+            Map<String, Object> thisOnes = getCreateIfNecessary(owner);
+            Object old = thisOnes.put(name, value);
+            if ("jrd.debug".equals(old)) {
+                System.err.println("We do not care about old.");
+            }
+            return value;
+        }
+
+        public static Object setNoReplace(Object owner, String name, Object value) throws NoSuchFakeVariableException {
+            Map<String, Object> thisOnes = getCreateIfNecessary(owner);
+            if (thisOnes.containsKey(name)) {
+                throw new FakeVariableAlreadyDeclaredException();
+            } else {
+                Object old = thisOnes.put(name, value);
+                if (old != null) {
+                    throw new IllegalStateException("Map is broken!");
+                }
+                return value;
+            }
+        }
+
+        public static Object get(Object owner, String name) throws NoSuchFakeVariableException {
+            Map<String, Object> thisOnes = getCreateIfNecessary(owner);
+            if (thisOnes.containsKey(name)) {
+                return thisOnes.get(name);
+            } else {
+                throw new NoSuchFakeVariableException();
+            }
+        }
+
+        public static Object getOrCreate(Object owner, String name, Object defaultValue) {
+            Map<String, Object> thisOnes = getCreateIfNecessary(owner);
+            if (thisOnes.containsKey(name)) {
+                return thisOnes.get(name);
+            } else {
+                Object old = thisOnes.put(name, defaultValue);
+                if (old != null) {
+                    throw new IllegalStateException("Map is broken!");
+                }
+                return defaultValue;
+            }
+        }
+
+        public static Object create(Object owner, String name, Object defaultValue) throws NoSuchFakeVariableException {
+            Map<String, Object> thisOnes = getCreateIfNecessary(owner);
+            if (thisOnes.containsKey(name)) {
+                throw new FakeVariableAlreadyDeclaredException();
+            } else {
+                return thisOnes.put(name, defaultValue);
+            }
+        }
+
+        public static Object remove(Object owner, String name) throws NoSuchFakeVariableException {
+            Map<String, Object> thisOnes = getCreateIfNecessary(owner);
+            if (thisOnes.containsKey(name)) {
+                return thisOnes.remove(name);
+            } else {
+                throw new NoSuchFakeVariableException();
+            }
+        }
+
+        public static void removeAll(Object owner) {
+            Map<String, Object> thisOnes = getCreateIfNecessary(owner);
+            thisOnes.clear();
+            LOCALS.remove(owner);
+        }
+
+        public static void removeAll() {
+            LOCALS.clear();
         }
     }
 }
