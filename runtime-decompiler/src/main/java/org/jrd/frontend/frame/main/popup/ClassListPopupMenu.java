ef76b498786045e701942b06defa86e7c9bc02e7
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-10-16 11:15:49 +0200

    Implemented right click class list interval selection
    Previously, multiline selection was removed for being confusing since only one class bytecode was loaded anyways.
    Also previously, the ability to copy class names (and subsequently class info) was introduced.
    This commit allows to select multiple classes, but only by holding RMB and only to bring up the Copy Popup menu.
    The selection then resets to what was selected before, which should correspond to the bytecode loaded in the buffers.

--- runtime-decompiler/src/main/java/org/jrd/frontend/frame/main/popup/ClassListPopupMenu.java
+++ runtime-decompiler/src/main/java/org/jrd/frontend/frame/main/popup/ClassListPopupMenu.java
@@ -3,39 +3,63 @@ package org.jrd.frontend.frame.main.popup;
 import org.jrd.backend.core.ClassInfo;
 
 import javax.swing.*;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public final class ClassListPopupMenu extends JPopupMenu {
 
     private ClassListPopupMenu() {
     }
 
-    public static ClassListPopupMenu create(ClassInfo classToCopy, boolean doShowClassInfo) {
+    public static ClassListPopupMenu create(JList<ClassInfo> parentJList, int originallySelected, boolean doShowClassInfo) {
+        List<ClassInfo> classesToCopy = parentJList.getSelectedValuesList();
         ClassListPopupMenu result = new ClassListPopupMenu();
 
-        result.add(createCopyItem("Copy class name", classToCopy.getName()));
+        result.add(createCopyItem("Copy class name", classesToCopy, ClassInfo::getName));
 
         if (doShowClassInfo) {
-            result.add(createCopyItem("Copy class location", classToCopy.getLocation()));
-            result.add(createCopyItem("Copy class loader", classToCopy.getClassLoader()));
+            result.add(createCopyItem("Copy class location", classesToCopy, ClassInfo::getLocation));
+            result.add(createCopyItem("Copy class loader", classesToCopy, ClassInfo::getClassLoader));
         }
+
+        result.addPopupMenuListener(new PopupMenuListener() {
+            @Override
+            public void popupMenuWillBecomeVisible(PopupMenuEvent popupMenuEvent) {
+            }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent popupMenuEvent) {
+                parentJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                parentJList.setSelectedIndex(originallySelected);
+                parentJList.requestFocusInWindow();
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent popupMenuEvent) {
+                // ...Canceled is called only when it gets dismissed, whereas ...WillBecomeInvisible is called everytime the menu disappears
+            }
+        });
 
         return result;
     }
 
-    private static JMenuItem createCopyItem(String itemTitle, String classProperty) {
+    private static JMenuItem createCopyItem(String itemTitle, List<ClassInfo> classProperties, Function<ClassInfo, String> mapFunction) {
         JMenuItem item = new JMenuItem(itemTitle);
 
         item.addActionListener(actionEvent -> {
-            if (classProperty == null || classProperty.isEmpty()) {
+            if (classProperties == null || classProperties.isEmpty()) {
                 return;
             }
 
-            StringSelection selection = new StringSelection(classProperty);
-            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            clipboard.setContents(selection, selection);
+            StringSelection selection = new StringSelection(
+                classProperties.stream().map(mapFunction).collect(Collectors.joining(System.getProperty("line.separator", "\n")))
+            );
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, selection);
         });
 
         return item;
