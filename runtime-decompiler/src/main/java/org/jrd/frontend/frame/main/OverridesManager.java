a521f1d3383a926c3ef891f7606a40283d828495
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-10-29 15:14:41 +0200

    Prettified OverridesManager code and look

--- runtime-decompiler/src/main/java/org/jrd/frontend/frame/main/OverridesManager.java
+++ runtime-decompiler/src/main/java/org/jrd/frontend/frame/main/OverridesManager.java
@@ -2,6 +2,7 @@ package org.jrd.frontend.frame.main;
 
 import org.jrd.frontend.utility.ScreenFinder;
 
+import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,10 +13,12 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -25,50 +28,38 @@ public final class OverridesManager {
 
     private static final OverridesManager DIALOG = new OverridesManager();
 
-    private static final String VM_TITLE = "Currently overridden classes in";
-    private final JLabel activeOverridesLabel = new JLabel(VM_TITLE + " XXXXXXXXXXXXXXXXXXX");
-    private final JList activeOverrides = new JList();
-    private final JButton refresh = new JButton("refresh");
-    private final JLabel removalLabel = new JLabel("Remove all overrides matching regex: ");
-    private final JTextField removalRegex = new JTextField(".*");
-    private final JButton remove = new JButton("Do!");
-    private final JButton close = new JButton("Close");
+    private static final String VM_TITLE = "Currently overridden classes in ";
+    private final JLabel activeOverridesLabel = new JLabel(VM_TITLE);
+    private final JList<String> activeOverrides;
+    private final JTextField removalRegex;
     private final JDialog window = new JDialog((JFrame) null, "Manage overridden classes");
     private DecompilationController dc;
 
     private OverridesManager() {
-        window.setLayout(new BorderLayout());
-        window.add(activeOverridesLabel, BorderLayout.NORTH);
-        window.add(new JScrollPane(activeOverrides));
+        window.setLayout(new BorderLayout(5, 5));
+        window.getRootPane().setBorder(BorderFactory.createCompoundBorder(window.getRootPane().getBorder(), new EmptyBorder(5, 5, 5, 5)));
+        window.setMinimumSize(new Dimension(400, 600));
+
+        activeOverrides = new JList<>();
         activeOverrides.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() >= 2) {
                     if (activeOverrides.getSelectedValue() != null) {
-                        removalRegex.setText(activeOverrides.getSelectedValue().toString());
+                        removalRegex.setText(activeOverrides.getSelectedValue());
                     } else {
                         removalRegex.setText(".*");
                     }
                 }
             }
         });
-        activeOverrides.setModel(new DefaultComboBoxModel() {
-            @Override
-            public Object getElementAt(int index) {
-                return "java.lang.hell.Hello";
-            }
+        activeOverrides.setMinimumSize(new Dimension(400, 400));
+        activeOverrides.setModel(new DefaultComboBoxModel<>());
 
-            @Override
-            public int getSize() {
-                return 10;
-            }
-        });
-        JPanel southPanel = new JPanel(new GridLayout(5, 1));
-        window.add(southPanel, BorderLayout.SOUTH);
-        southPanel.add(refresh);
-        refresh.addActionListener(a -> load());
-        southPanel.add(removalLabel);
-        southPanel.add(removalRegex);
+        JButton refresh = new JButton("Refresh");
+        refresh.addActionListener(a -> loadOverrides());
+
+        removalRegex = new JTextField(".*");
         final Color originalForeground = removalRegex.getForeground();
         final Color originalBackground = removalRegex.getBackground();
         removalRegex.getDocument().addDocumentListener(new DocumentListener() {
@@ -96,12 +87,24 @@ public final class OverridesManager {
                 removeUpdate(documentEvent);
             }
         });
-        southPanel.add(remove);
-        remove.addActionListener(a -> doRemove());
-        southPanel.add(close);
+
+        JButton remove = new JButton("Remove!");
+        remove.addActionListener(a -> removeOverride());
+
+        JButton close = new JButton("Close");
         close.addActionListener(a -> hide());
+
+        JPanel southPanel = new JPanel(new GridLayout(5, 1, 5, 5));
+        southPanel.add(refresh);
+        southPanel.add(new JLabel("Remove all overrides matching regex: "));
+        southPanel.add(removalRegex);
+        southPanel.add(remove);
+        southPanel.add(close);
+
+        window.add(activeOverridesLabel, BorderLayout.NORTH);
+        window.add(new JScrollPane(activeOverrides), BorderLayout.CENTER);
+        window.add(southPanel, BorderLayout.SOUTH);
         window.pack();
-        activeOverrides.setModel(new DefaultComboBoxModel());
         ScreenFinder.centerWindowToCurrentScreen(window);
     }
 
@@ -114,26 +117,27 @@ public final class OverridesManager {
             JOptionPane.showMessageDialog(parent, "No VM selected!");
         } else {
             DIALOG.setDc(dc);
-            DIALOG.load();
+            DIALOG.loadOverrides();
             // always per vm, so modal
             DIALOG.window.setModal(true);
             DIALOG.window.setVisible(true);
         }
     }
 
-    private void doRemove() {
+    private void removeOverride() {
         try {
             dc.removeOverrides(Pattern.compile(removalRegex.getText()).toString());
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
         }
-        load();
+
+        loadOverrides();
     }
 
-    private void load() {
+    private void loadOverrides() {
         try {
             String[] classes = dc.getOverrides();
-            activeOverrides.setModel(new DefaultComboBoxModel(classes));
+            activeOverrides.setModel(new DefaultComboBoxModel<>(classes));
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
         }
@@ -142,6 +146,6 @@ public final class OverridesManager {
 
     private void setDc(DecompilationController dc) {
         this.dc = dc;
-        activeOverridesLabel.setText(VM_TITLE + " " + dc.getVm());
+        activeOverridesLabel.setText(VM_TITLE + dc.getVm() + ":");
     }
 }
