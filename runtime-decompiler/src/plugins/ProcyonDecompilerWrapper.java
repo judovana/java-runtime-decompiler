759882d766ad635b3a37038f28177fabfb800fea
Jiri Vanek <jvanek@redhat.com>
2022-02-08 18:54:07 +0100

    Adapted procyon wrapper to 0.6
    added help and multiclass

--- runtime-decompiler/src/plugins/ProcyonDecompilerWrapper.java
+++ runtime-decompiler/src/plugins/ProcyonDecompilerWrapper.java
@@ -1,10 +1,15 @@
 import com.strobel.decompiler.Decompiler;
 import com.strobel.decompiler.PlainTextOutput;
+import com.strobel.decompiler.DecompilerDriver;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.util.Map;
 
 public class ProcyonDecompilerWrapper {
 
@@ -27,6 +32,30 @@ public class ProcyonDecompilerWrapper {
         return writer.toString();
     }
 
+    public String decompile(
+            String name, byte[] bytecode, Map<String, byte[]> innerClasses, String[] options
+    ) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        PrintStream so = System.out;
+        System.setOut(new PrintStream(bos));
+        try {
+            String[] files = new String[innerClasses.size() + 1];
+            files[0] = bytesToFile(bytecode).getAbsolutePath();
+            int i = 1;
+            for (byte[] inner : innerClasses.values()) {
+                files[i] = bytesToFile(inner).getAbsolutePath();
+                i++;
+            }
+            DecompilerDriver.main(files);
+            return bos.toString(Charset.defaultCharset());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        } finally {
+            System.setOut(so);
+        }
+    }
+
     private File bytesToFile(byte[] bytes) throws IOException {
         File tempFile = File.createTempFile("temporary-byte-file", ".class");
         tempFile.deleteOnExit();
@@ -34,5 +63,20 @@ public class ProcyonDecompilerWrapper {
         fos.write(bytes);
         fos.close();
         return tempFile;
+    }
+
+    public String decompilerHelp() throws IOException {
+        PrintStream oldo = System.out;
+        PrintStream olde = System.err;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        System.setErr(new PrintStream(bos));
+        try {
+            DecompilerDriver.main(new String[]{"--help"});
+            return bos.toString("utf-8");
+        } finally {
+            System.setOut(oldo);
+            System.setOut(olde);
+        }
     }
 }
