6ca685b8eefac7494853e574faa10cc360991bd7
Jiri Vanek <jvanek@redhat.com>
2022-10-24 17:38:36 +0200

    Forgoten wrapper class

---
+++ runtime-decompiler/src/main/java/org/jrd/backend/data/cli/PluginWrapperWithMetaInfo.java
@@ -0,0 +1,19 @@
+package org.jrd.backend.data.cli;
+
+public class PluginWrapperWithMetaInfo {
+    private final PluginWithOptions wrapper;
+    private final boolean haveCompiler;
+
+    public PluginWrapperWithMetaInfo(PluginWithOptions wrapper, boolean haveCompiler) {
+        this.wrapper = wrapper;
+        this.haveCompiler = haveCompiler;
+    }
+
+    public PluginWithOptions getWrapper() {
+        return wrapper;
+    }
+
+    public boolean haveCompiler() {
+        return haveCompiler;
+    }
+}
