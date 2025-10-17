a6d6f940f41ab43970f818f2f6e5683d25e66c79
Jiri Vanek <jvanek@redhat.com>
2025-10-17 17:10:15 +0200

    Fixing the write to static field from instance method findbugs warning

--- runtime-decompiler/src/main/java/org/jrd/frontend/frame/remote/NewConnectionView.java
+++ runtime-decompiler/src/main/java/org/jrd/frontend/frame/remote/NewConnectionView.java
@@ -1,7 +1,5 @@
 package org.jrd.frontend.frame.remote;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
 import org.jrd.backend.communication.CallDecompilerAgent;
 import org.jrd.frontend.frame.main.MainFrameView;
 
@@ -32,8 +30,8 @@ public class NewConnectionView extends JDialog {
 
     public static class HostnamePortInputPanel extends JPanel {
 
-        private static String lastHost = CallDecompilerAgent.DEFAULT_ADDRESS;
-        private static String lastPort = "10900";
+        private static volatile String lastHost = CallDecompilerAgent.DEFAULT_ADDRESS;
+        private static volatile String lastPort = "10900";
 
         JTextField hostnameTextField;
         JTextField portTextField;
@@ -145,7 +143,6 @@ public class NewConnectionView extends JDialog {
         return hostnamePortInputPanel.hostnameTextField.getText();
     }
 
-    @SuppressFBWarnings(value = "ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD", justification = "intentional")
     void reSetLastHostname() {
         HostnamePortInputPanel.lastHost = hostnamePortInputPanel.hostnameTextField.getText();
     }
@@ -154,7 +151,6 @@ public class NewConnectionView extends JDialog {
         return hostnamePortInputPanel.portTextField.getText();
     }
 
-    @SuppressFBWarnings(value = "ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD", justification = "intentional")
     void reSetLastPort() {
         HostnamePortInputPanel.lastPort = hostnamePortInputPanel.portTextField.getText();
     }
